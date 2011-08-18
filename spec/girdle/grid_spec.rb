04473198db7c5adee35563ad0781f50c158c8780
Jamie Hodge <jamiehodge@me.com>
2011-08-19 01:22:50 +0200

    Fixed incorrect constant

--- spec/girdle/grid_spec.rb
+++ spec/girdle/grid_spec.rb
@@ -1,22 +1,22 @@
 require_relative '../spec_helper'
 
-describe Griddle::Grid do
+describe Girdle::Grid do
   
   it 'must have id attribute' do
-    grid = Griddle::Grid.new(123)
+    grid = Girdle::Grid.new(123)
     grid.id.must_equal 123
   end
   
   it 'must retrieve list identifiers of all logical grids' do
-    Griddle.expects(:run).with(grid: 'list').returns([1,2,3])
-    list = Griddle::Grid.list
+    Girdle.expects(:run).with(grid: 'list').returns([1,2,3])
+    list = Girdle::Grid.list
     list.must_equal [1,2,3]
   end
   
   describe 'a grid' do
     
     before do
-      @grid = Griddle::Grid.new(123)
+      @grid = Girdle::Grid.new(123)
       @attributes = {
         'name' => 'name',
         'gridMegahertz' => '0',
@@ -25,7 +25,7 @@ describe Griddle::Grid do
     end
     
     it 'must retrieve attributes' do
-      Griddle.expects(:run).with(grid: 'attributes', gid: 123).returns('gridAttributes' => @attributes)
+      Girdle.expects(:run).with(grid: 'attributes', gid: 123).returns('gridAttributes' => @attributes)
       @grid.attributes.must_equal @attributes
     end
     
