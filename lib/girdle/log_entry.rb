776e997d9b27cd20bbef3e152742980aa9526f2d
Jamie Hodge <jamiehodge@me.com>
2011-10-07 11:18:53 +0200

    Fallback to string hash keys

--- lib/girdle/log_entry.rb
+++ lib/girdle/log_entry.rb
@@ -2,8 +2,8 @@ module Girdle
   class LogEntry
     
     def initialize(entry)
-      @message = entry[:message]
-      @time = entry[:time]
+      @message = entry[:message] || entry['message']
+      @time = entry[:time] || entry['time']
     end
 
     attr_reader :message, :time
